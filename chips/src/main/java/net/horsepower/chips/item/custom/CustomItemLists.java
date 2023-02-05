5f4dd8b084df42dc1b774e92f516a65ba60e1a2c
TheFastTheHackerTheTilted <spamjunkemir@gmail.com>
2023-02-05 12:17:47 +0100

    chips packs added

--- chips/src/main/java/net/horsepower/chips/item/custom/CustomItemLists.java
+++ chips/src/main/java/net/horsepower/chips/item/custom/CustomItemLists.java
@@ -12,6 +12,21 @@ public class CustomItemLists {
     static int commonCount = ModConfigs.commonCount;
     static List<Integer> commonList = listCommon();
 
+    static int rareCount = ModConfigs.rareCount;
+    static List<Integer> rareList = listRare();
+
+
+
+    static int epicCount = ModConfigs.epicCount;
+    static List<Integer> epicList = listEpic();
+
+
+
+    static int legendaryCount = ModConfigs.legendaryCount;
+    static List<Integer> legendaryList = listLegendary();
+
+
+
 
     public static List<Integer> listCommon(){
         String list = ModConfigs.commonItemList;
@@ -24,6 +39,37 @@ public class CustomItemLists {
         return res;
     }
 
+    private static List<Integer> listRare() {
+        String list = ModConfigs.rareItemList;
+        String[] strResult = list.split(",");
+        List<Integer> res = new ArrayList<>();
+        for (String s : strResult){
+            res.add(Integer.parseInt(s));
+        }
 
+        return res;
+    }
+
+    private static List<Integer> listEpic() {
+        String list = ModConfigs.epicItemList;
+        String[] strResult = list.split(",");
+        List<Integer> res = new ArrayList<>();
+        for (String s : strResult){
+            res.add(Integer.parseInt(s));
+        }
+
+        return res;
+    }
+
+    private static List<Integer> listLegendary() {
+        String list = ModConfigs.legendaryItemList;
+        String[] strResult = list.split(",");
+        List<Integer> res = new ArrayList<>();
+        for (String s : strResult){
+            res.add(Integer.parseInt(s));
+        }
+
+        return res;
+    }
 
 }
---
+++ chips/src/main/java/net/horsepower/chips/item/custom/RewardBagRare.java
@@ -0,0 +1,43 @@
+package net.horsepower.chips.item.custom;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+import java.util.List;
+import java.util.Random;
+
+
+public class RewardBagRare extends Item {
+    public RewardBagRare(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        //use random
+        Random rds = new Random();
+        int count = rds.nextInt(CustomItemLists.rareCount) +1;
+
+        //get the list
+        List<Integer> list = CustomItemLists.rareList;
+        int rItem = list.get(rds.nextInt(list.size()));
+
+        //get main hand
+        ItemStack itemHeld = user.getStackInHand(hand);
+
+
+        if (!world.isClient() && hand == Hand.MAIN_HAND){
+            user.sendMessage(Text.literal(count+" of id:"+rItem+" is given!"));
+            itemHeld.decrement(1);
+            user.dropStack(new ItemStack(Item.byRawId(rItem),count));
+            return TypedActionResult.success(itemHeld);
+        }
+
+        return TypedActionResult.success(itemHeld);
+    }
+}
