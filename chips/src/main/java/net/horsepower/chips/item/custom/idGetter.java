2edd8b8f156a519f2391d8623a156baf433c216c
TheFastTheHackerTheTilted <spamjunkemir@gmail.com>
2023-02-05 12:54:52 +0100

    added tooltip

--- chips/src/main/java/net/horsepower/chips/item/custom/idGetter.java
+++ chips/src/main/java/net/horsepower/chips/item/custom/idGetter.java
@@ -1,12 +1,17 @@
 package net.horsepower.chips.item.custom;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class idGetter extends Item {
     public idGetter(Settings settings) {
@@ -26,5 +31,11 @@ public class idGetter extends Item {
             }
         }
         return TypedActionResult.fail(offHandItem);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.literal("DEVTOOL: Prints the ID of whats in off-hand").formatted(Formatting.AQUA));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
